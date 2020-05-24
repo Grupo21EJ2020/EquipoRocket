798c302c7e439c0d6e280d7d084ef5e99f4a8fec
Juan De Dios Nacianceno <hackmasterpro1000@gmail.com>
2020-05-23 19:58:17 -0500

    orden

--- funciones.py
+++ funciones.py
@@ -15,7 +15,23 @@ class Cursos_Temas:
         idtema = input("> ")
 
         archivo.write(idcursotema + "|" + idcurso + "|" + idtema)
-        archivo.close
+            archivo.close
+
+     #Se agrega la informacion al archivo empleados.txt
+class Info():
+    def __init__(self, idEmpleado, Nombre, Direccion):
+        self.idEmpleado = idEmpleado
+        self.Nombre = Nombre 
+        self.Direccion = Direccion  
+
+archivo = open("./archivos/empleados.txt","a",encoding='utf8')
+
+idEmpleado = input("Numero de registro:\n")
+Nombre = input("Nombre del Empleado:\n")
+Direccion = input("Direccion del Empleado:\n")
+
+archivo.write(idEmpleado + "|" + Nombre + "|" + Direccion)
+archivo.close()
 
 
 
@@ -185,24 +201,3 @@ class Video:
     @url.setter
     def url (self,valor):
         self.__url = valor
-    
-        
-    
-
-=======
-#Se agrega la informacion al archivo empleados.txt
-class Info():
-    def __init__(self, idEmpleado, Nombre, Direccion):
-        self.idEmpleado = idEmpleado
-        self.Nombre = Nombre 
-        self.Direccion = Direccion  
-
-archivo = open("./archivos/empleados.txt","a",encoding='utf8')
-
-idEmpleado = input("Numero de registro:\n")
-Nombre = input("Nombre del Empleado:\n")
-Direccion = input("Direccion del Empleado:\n")
-
-archivo.write(idEmpleado + "|" + Nombre + "|" + Direccion)
-archivo.close()
-
