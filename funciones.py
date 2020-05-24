2300c79cdf163c5867744546afde96a741da3bd0
SOFIA QUIROZ <SofiaMonserrat524@hotmail.com>
2020-05-23 19:44:48 -0500

    video

--- funciones.py
+++ funciones.py
@@ -121,3 +121,49 @@ def menu_principal():
         else:
             print("Opcion no valida")
 
+
+class Video:
+    def __init__(self,idVideo,nombre,fechapublicacion,url):
+        self.__idVideo = idVideo 
+        self.__nombre = nombre
+        self.__fechapublicacion = fechapublicacion
+        self.__url= url
+    
+    def fromstring(cls,cadena):
+        idVideo,nombre,fechapublicacion,url = cadena.split(",")
+        return cls (int(idVideo),nombre,str(fechapublicacion),url)
+    def fromstring(cls,lista):
+        idVideo,nombre,fechapublicacion,url = lista.join(" ")
+        return cls (int(idVideo),nombre,str(fechapublicacion),url)
+    
+    @property
+    def idVideo(self):
+        return self.__idVideo
+    @property
+    def nombre (self):
+        return self.__nombre
+    @property
+    def fechapublicacion (self):
+        return self.__fechapublicacion
+    @property
+    def url (self):
+        return self.__url
+    
+    @idVideo.setter
+    def idVideo(self,valor):
+        self.__idVideo = valor
+        
+    @nombre.setter
+    def nombre (self,valor):
+        self.__nombre=valor
+    
+    @fechapublicacion.setter
+    def fechapublicacion (self,valor):
+        self.__fechapublicacion=valor
+    @url.setter
+    def url (self,valor):
+        self.__url = valor
+    
+        
+    
+
