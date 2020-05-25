44555c7f8b5bf2c4b92fe77a947ae5e7746f0537
Omar Gutierrez Garcia <omarg.garcia.1999@gmail.com>
2020-05-25 10:39:42 -0500

    Se agrego funcion modificar

--- cursos_temas.py
+++ cursos_temas.py
@@ -1,4 +1,6 @@
-class Cursos_Temas:
+import os
+
+class Curso_Tema:
     def __init__(self,idCursoTema,idCurso,idTema):
         self.idCursoTema = idCursoTema
         self.idCurso = idCurso
@@ -37,3 +39,36 @@ class Cursos_Temas:
                     break
                 else:
                     print(renglon)
+                    break
+
+        archivo.close()
+
+    def modificar_cursos_temas():
+        archivo = open("./archivos/cursos_temas.txt","r",encoding="utf8")
+        archivo_temp = open("./archivos/cursos_temas_temp.txt","w",encoding="utf8")
+
+        print("Dime el id que quieres modificar")
+        id_mod = input("> ")
+        print("Dime el nuevo id del tema del curso")
+        idcursotema = input("> ")
+        print("Dime el nuevo id del curso")
+        idcurso = input("> ")
+        print("Dime el nuevo id del tema")
+        idtema = input("> ")
+
+        for renglon in archivo:
+            for x in renglon:
+                if id_mod != x:
+                    archivo_temp.write(renglon)
+                    break
+                elif id_mod == x:
+                    archivo_temp.write(idcursotema + "|" + idcurso + "|" + idtema + "\n")
+                    break
+    
+        archivo.close()
+        archivo_temp.close()
+        os.remove("./archivos/cursos_temas.txt")
+        os.rename("./archivos/cursos_temas_temp.txt","./archivos/cursos_temas.txt")
+
+
+Curso_Tema.agregar_curso_tema()
