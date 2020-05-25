36e9576dba8eef0d86f6871115a86226ed517dac
Juan De Dios Nacianceno <hackmasterpro1000@gmail.com>
2020-05-24 19:17:47 -0500

    Se creo archivo empleados

--- funciones.py
+++ funciones.py
@@ -39,39 +39,6 @@ class Cursos_Temas:
                     print(renglon)
 
 
-     #Se agrega la informacion al archivo empleados.txt
-
-class Info_Empleado:
-    def __init__(self,idEmpleado,Nombre,Direccion):
-        self.idEmpleado = idEmpleado
-        self.Nombre = Nombre
-        self.Direccion = Direccion 
-
-    def AgregarEmpleado():
-        print "Registro de Empleado"
-        archivo = open("./archivos/empleados.txt","a",encoding="utf8")
-
-        print("Clave del Empleado Nuevo")
-        idempleado = input("Id \n")
-        print("Nombre del Empleado:\n")
-        nombre = input("Nombre: \n")
-        print("Direccion del Empleado")
-        direccion = input("> ")
-
-        archivo.write(idempleado + "|" + nombre + "|" + direccion)
-        
-        archivo.close()
-
-
-    def consultar_empleado():
-        archivo = open("./archivos/empleados.txt",encoding="utf8")
-
-        print(archivo.read())
-
-        archivo.close()
-
-
-
 def opciones_modificacion():
     print("¿Que opcion quieres realizar?")
     print("1. Agregar")
