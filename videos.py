f8a70cc93466b49abb5d6506ad2feb57f28637bb
SOFIA QUIROZ <SofiaMonserrat524@hotmail.com>
2020-05-26 18:19:55 -0500

    Agregue opciones de video

--- videos.py
+++ videos.py
@@ -42,16 +42,26 @@ class Video:
         self.__url = valor
 
 
-archivo = open ("./archivo/video.txt","a",encoding = 'utf8')
-idVideo = int (input ("clave del video"))
-nombre=input("nombre del video")
-fechadepublicacion=input("fecha de publicacion del video")
-url = input("Dame tu url del video")
-archivo.write(idVideo + "/" + nombre + "/" + fechadepublicacion + "/" + url)
-archivo.close()
 
-    def  consultar_video ():
-        archivo  =  open ( "./archivos/video.txt" , encoding = "utf8" )
+
+
+    def agregar_video(self):
+        self.archivo = open ("./archivos/video.txt","a",encoding = 'utf8')
+    
+        print("Dime el id del tema de tu video")
+        self.idVideo = input("> ")
+        print("Dime el id del curso")
+        self.nombre = input("> ")
+        print ("Dime la fecha de publicacion")
+        self.fechadepublicacion = input ("> ")
+        print ("Dime la url")
+        self.url = input ("> ")
+
+        self.archivo.write(self.idVideo + "|" + self.nombre + "|" + self.fechadepublicacion +"|"+ self.url + "|"+ "\n")
+        
+        self.archivo.close()
+    def  consultar_video (self):
+        self.archivo  =  open ( "./archivos/video.txt" , encoding = "utf8" )
 
         print ( archivo . read ())
 
@@ -59,15 +69,60 @@ archivo.close()
 
 
     def  detalle_video ():
-        archivo  =  abierto ( "./archivos/empleados.txt" , codificación = "utf8" )
+        archivo  =  open  ( "./archivos/video.txt" , encoding = "utf8" )
 
         print ( "Id del video a buscar" )
         idVideo earch  =  input ( "Nombre del video:" )
 
-        for  renglon  in  archivo :
-            for   x  in  renglon :
-                if  id_empleadosearch  ! =  x :
+        for  linea  in  archivo :
+            for   x  in  linea :
+                if  idVideosearch  ! =  x :
                     break
                 else :
-                    print ( renglon )
+                    print ( linea )
+        archivo . close ()
+
+    def modificar_video(self):
+        self.archivo = open("./archivos/video.txt","r",encoding="utf8")
+        
+
+        print("Dime el id  del video que quieres modificar")
+        self.idVideo_mod = input("> ")
+        print("Dime el nuevo nombre del video")
+        self.nombre = input("> ")
+        print("Dime la fecha de publicacion nueva ")
+        self.fechadepublicacion = input("> ")
+        print("Dime la nueva url ")
+        self.url = input("> ")
+
+        for renglon in self.archivo:
+            id = renglon.split("|")[0]
+            if self.idVideo_mod != id:
+                self.archivo_temp.write(renglon)
+            elif self.idVideo_mod == id:
+                self.archivo.write(self.idVideo_mod + "|" + self.nombre + "|" + self.fechadepublicacion +"|" + self.url "\n")
     
+        self.archivo.close()
+        
+        os.remove("./archivos/video.txt")
+    
+
+    def borrar_video(self):
+        self.archivo = open("./archivos/video.txt","r",encoding="utf8")
+        self.archivo_2 = open("./archivos/video2.txt","w",encoding="utf8")
+
+        print("Dime el id del video que quieres borrar")
+        self.idVideo_borrar = input("> ")
+
+        for renglon in self.archivo:
+            id = renglon.split("|")[0]
+            if self.idVideo_borrar != id:
+                self.archivo_2.write(renglon)
+    
+        self.archivo.close()
+        self.archivo_2.close()
+        os.remove("./archivos/video.txt")
+        os.rename("./archivos/video2.txt")
+
+V = Video_elegido(0,0,0)
+V.borrar_Video ()
