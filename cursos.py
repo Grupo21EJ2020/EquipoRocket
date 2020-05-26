146d1be5bb6e69caf7412a3c81d500e5be62e03d
Paulina Mendoza <Michelle_1068@outlook.es>
2020-05-26 11:41:49 -0500

    Se agrego la opcion de Borrar Curso

--- cursos.py
+++ cursos.py
@@ -18,7 +18,28 @@ class Curso():
         
         archivo.close()
 
-        
+
+    def borrar_curso(self):
+        self.archivo = open("./archivos/cursos.txt","r",encoding="utf8")
+        self.archivo_temp = open("./archivos/cursos_temp.txt","w",encoding="utf8")
+
+        print("Dime el id que quieres borrar")
+        self.id_borrar = input("> ")
+
+        for renglon in self.archivo:
+            id = renglon.split("|")[0]
+            if self.id_borrar != id:
+                self.archivo_temp.write(renglon)
+    
+        self.archivo.close()
+        self.archivo_temp.close()
+        os.remove("./archivos/cursos.txt")
+        os.rename("./archivos/cursos_temp.txt","./archivos/cursos_temas.txt")
+
+    A = Curso(0,0,0)
+    A.borrar_curso()
+
+
     def consultar_curso_():
         archivo = open("./archivos/cursos.txt",encoding="utf8")
 
