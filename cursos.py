11931d06f916f8296758c12709eb8cd4b42f8822
Paulina Mendoza <Michelle_1068@outlook.es>
2020-05-26 11:50:09 -0500

    Agregue opcion Modificar Curso

--- cursos.py
+++ cursos.py
@@ -18,7 +18,6 @@ class Curso():
         
         archivo.close()
 
-
     def borrar_curso(self):
         self.archivo = open("./archivos/cursos.txt","r",encoding="utf8")
         self.archivo_temp = open("./archivos/cursos_temp.txt","w",encoding="utf8")
@@ -39,6 +38,30 @@ class Curso():
     A = Curso(0,0,0)
     A.borrar_curso()
 
+    def modificar_curso(self):
+        self.archivo = open("./archivos/cursos.txt","r",encoding="utf8")
+        self.archivo_temp = open("./archivos/cursos_temp.txt","w",encoding="utf8")
+
+        print("Dime el id que quieres modificar")
+        self.id_mod = int(input("> "))
+        print("Dime el nuevo id del curso")
+        self.idCurso = int(input("> "))
+        print("Dime la nueva descripcion")
+        self.descripcion = input("> ")
+        print("Dime la nueva id del empleado")
+        self.idEmpleado = int(input("> "))
+
+        for renglon in self.archivo:
+            id = renglon.split("|")[0]
+            if self.id_mod != id:
+                self.archivo_temp.write(renglon)
+            elif self.id_mod == id:
+                self.archivo_temp.write(self.idCurso + "|" + self.descripcion + "|" + self.idEmpleado + "\n")
+    
+        self.archivo.close()
+        self.archivo_temp.close()
+        os.remove("./archivos/cursos.txt")
+        os.rename("./archivos/cursos_temp.txt","./archivos/cursos.txt")
 
     def consultar_curso_():
         archivo = open("./archivos/cursos.txt",encoding="utf8")
