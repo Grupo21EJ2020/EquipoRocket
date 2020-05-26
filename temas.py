0fe29d89e980cf3b311a1c7db90057b177f5b05c
Paulina Mendoza <Michelle_1068@outlook.es>
2020-05-26 11:58:35 -0500

    Se modifico la clase Temas

--- temas.py
+++ temas.py
@@ -3,18 +3,3 @@ class temas:
         self.IdTema = IdTema
         self.Tema = Tema
 
-    @property
-    def idTema(self):
-        return self.__idTema
-    
-    @idTema.setter
-    def idTema(self,valor):
-        self.__idTema = valor
-
-    @property
-    def tema(self):
-        return self.__tema
-    
-    @tema.setter
-    def idtema(self,valor):
-        self._tema = valor
